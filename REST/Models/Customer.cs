dcf13eec8b44c4583282ec88ba1998dc1db4fb5e
Fahri Hanif <fahrihanif03@gmail.com>
2022-09-19 14:47:29 +0700

    Define a database

--- REST/Models/Customer.cs
+++ REST/Models/Customer.cs
@@ -1,6 +1,23 @@
-﻿namespace REST.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace REST.Models
 {
+    [Table("tb_m_customer")]
     public class Customer
     {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name"), MaxLength(150), Required]
+        public string Name { get; set; }
+        [Column("address"), MaxLength(150), Required]
+        public string Address { get; set; }
+        [Column("city"), MaxLength(50), Required]
+        public string City { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Placement> Placements { get; set; }
     }
 }
--- REST/Models/Employee.cs
+++ REST/Models/Employee.cs
@@ -1,6 +1,38 @@
-﻿namespace REST.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace REST.Models
 {
+    [Table("tb_m_employee")]
     public class Employee
     {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("first_name"), MaxLength(50), Required]
+        public string FirstName { get; set; }
+        [Column("last_name"), MaxLength(50)]
+        public string LastName { get; set; }
+        [Column("birth_date"), Required]
+        public DateTime BirthDate { get; set; }
+        [Column("gender"), Required]
+        public Gender Gender { get; set; }
+        [Column("status"), MaxLength(50), Required]
+        public string Status { get; set; }
+        [Column("email"), MaxLength(50), Required]
+        public string Email { get; set; }
+        [Column("email_company"), MaxLength(50)]
+        public string EmailCompany { get; set; }
+        [Column("phone"), MaxLength(15), Required]
+        public string Phone { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Interview> Interviews { get; set; }
+    }
+
+    public enum Gender
+    {
+        MALE, FEMALE
     }
 }
