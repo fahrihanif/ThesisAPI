b2657e50271f1dcbb47ac825443ce6cde6f4616d
Fahri Hanif <fahrihanif03@gmail.com>
2022-09-19 17:19:41 +0700

    set up crud on graphql project

---
+++ GraphQL/Context/Mutation.cs
@@ -0,0 +1,33 @@
+﻿using GraphQL.Models;
+
+namespace GraphQL.Context
+{
+    public class Mutation
+    {
+        public async Task<Interview> SaveInterview([Service] MyContext context, Interview newInterview)
+        {
+            context.Interviews.Add(newInterview);
+            await context.SaveChangesAsync();
+            return newInterview;
+        }
+
+        public async Task<Interview> UpdateInterview([Service] MyContext context, Interview updateInterview)
+        {
+            context.Interviews.Update(updateInterview);
+            await context.SaveChangesAsync();
+            return updateInterview;
+        }
+
+        public async Task<string> DeleteInterview([Service] MyContext context, int id)
+        {
+            var interviewToDelete = await context.Interviews.FindAsync(id);
+            if (interviewToDelete == null)
+            {
+                return "Invalid Operation";
+            }
+            context.Interviews.Remove(interviewToDelete);
+            await context.SaveChangesAsync();
+            return "Record Deleted!";
+        }
+    }
+}
---
+++ GraphQL/Context/Query.cs
@@ -0,0 +1,10 @@
+﻿using GraphQL.Models;
+
+namespace GraphQL.Context
+{
+    public class Query
+    {
+        [UseProjection]
+        public IQueryable<Interview> interviews([Service] MyContext context) => context.Interviews;
+    }
+}
